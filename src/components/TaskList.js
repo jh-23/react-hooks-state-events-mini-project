dd5a2839157716cfdc26ab6b9d70a6f039607147
John Heger <heger7@gmail.com>
2023-12-03 19:16:14 -0600

    completed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { TASKS } from "../data";
+import Task from "./Task";
 
-function TaskList() {
+
+
+function TaskList( {tasks, setDeletedTask, handleDelete } ) {
+
+  const listOfTasks = tasks.map((task) => {
+    return (
+      <Task key={task.text} text={task.text} category={task.category} setDeletedTask={setDeletedTask} handleDelete={handleDelete} />
+    )
+  })
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {listOfTasks}
     </div>
   );
 }
